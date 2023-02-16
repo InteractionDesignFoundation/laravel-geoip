38e507c7f8669b1d36f31825c2ae5a3afdc509ec
Mateus Junges <mateus@junges.dev>
2023-02-16 19:46:44 -0300

    Add methods to interface

--- src/Contracts/Client.php
+++ src/Contracts/Client.php
@@ -4,7 +4,12 @@ namespace InteractionDesignFoundation\GeoIP\Contracts;
 
 interface Client
 {
+    /** Set the HTTP client configuration. */
     public function setConfig(array $config): self;
 
+    /** Set the parameters that should be sent in every request. */
+    public function setDefaultQueryParameters(array $query): self;
+
+    /** Performs a GET request. */
     public function get(string $url, array $query = [], array $headers = []): array;
 }
