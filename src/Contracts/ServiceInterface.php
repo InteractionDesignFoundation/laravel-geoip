5c28076292f6d5f631786f27fcad704283f2a3fc
Mateus Junges <mateus@junges.dev>
2023-02-16 19:47:00 -0300

    Add types

--- src/Contracts/ServiceInterface.php
+++ src/Contracts/ServiceInterface.php
@@ -2,41 +2,25 @@
 
 namespace InteractionDesignFoundation\GeoIP\Contracts;
 
+use InteractionDesignFoundation\GeoIP\Location;
+
 interface ServiceInterface
 {
-    /**
-     * The "booting" method of the service.
-     *
-     * @return void
-     */
-    public function boot();
+    /** The "booting" method of the service. */
+    public function boot(): void;
 
-    /**
-     * Determine a location based off of
-     * the provided IP address.
-     *
-     * @param string $ip
-     *
-     * @return \InteractionDesignFoundation\GeoIP\Location
-     */
-    public function locate($ip);
+    /** Determine a location based off of the provided IP address. */
+    public function locate(string $ip): Location;
 
-    /**
-     * Create a location instance from the provided attributes.
-     *
-     * @param array $attributes
-     *
-     * @return \InteractionDesignFoundation\GeoIP\Location
-     */
-    public function hydrate(array $attributes = []);
+    /** Create a location instance from the provided attributes. */
+    public function hydrate(array $attributes = []): Location;
 
     /**
      * Get configuration value.
      *
-     * @param string $key
      * @param mixed  $default
      *
      * @return mixed
      */
-    public function config($key, $default = null);
+    public function config(string $key, $default = null);
 }
