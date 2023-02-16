8a62b9d1386f99cb7a3dfdab4afb005dc3c0286d
Mateus Junges <mateus@junges.dev>
2023-02-16 20:16:26 -0300

    Simplify code

--- src/Services/IPGeoLocation.php
+++ src/Services/IPGeoLocation.php
@@ -4,52 +4,38 @@ namespace InteractionDesignFoundation\GeoIP\Services;
 
 use Exception;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\App;
+use InteractionDesignFoundation\GeoIP\Contracts\Client;
+use InteractionDesignFoundation\GeoIP\Location;
 use InteractionDesignFoundation\GeoIP\Support\HttpClient;
 
 class IPGeoLocation extends AbstractService
 {
-    /**
-     * Http client instance.
-     *
-     * @var HttpClient
-     */
-    protected $client;
+    /** Http client instance. */
+    protected Client $client;
 
-    /**
-     * The "booting" method of the service.
-     *
-     * @return void
-     */
-    public function boot()
+    /** The "booting" method of the service.  */
+    public function boot(): void
     {
-        $base = [
-            'base_uri' => 'https://api.ipgeolocation.io/',
-        ];
+        $this->client = App::make(Client::class);
+
+        $baseUri = 'https://api.ipgeolocation.io/';
+        $this->client->setConfig([
+            'base_uri' => $baseUri,
+        ]);
 
         if ($this->config('key')) {
-            $base['base_uri'] = "{$base['base_uri']}ipgeo?apiKey=" . $this->config('key');
+            $this->client->setConfig([
+                'base_uri' => "{$baseUri}ipgeo?apiKey=" . $this->config('key')
+            ]);
         }
-
-        $this->client = new HttpClient($base);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-
-    public function locate($ip)
+    public function locate(string $ip): Location
     {
         // Get data from client
-        $data = $this->client->get('&ip=' . $ip);
+        $data = $this->client->get('/', ['ip' => $ip]);
 
-        // Verify server response
-        if ($this->client->getErrors() !== null) {
-            throw new Exception('Request failed (' . $this->client->getErrors() . ')');
-        }
-
-        // Parse body content
-        $json = json_decode($data[0], true);
-
-        return $this->hydrate($json);
+        return $this->hydrate($data);
     }
 }
