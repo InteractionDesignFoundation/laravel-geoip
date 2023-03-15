079d8ffe62d6b0d9a14d25eb53691db896106079
Mateus <mateus@junges.dev>
2023-03-15 16:34:20 -0300

    Set psalm baseline

--- src/helpers.php
+++ src/helpers.php
@@ -4,16 +4,16 @@ if (!function_exists('geoip')) {
     /**
      * Get the location of the provided IP.
      *
-     * @param string $ip
-     *
-     * @return \InteractionDesignFoundation\GeoIP\GeoIP|\InteractionDesignFoundation\GeoIP\Location
+     * @throws \Psr\SimpleCache\InvalidArgumentException
      */
-    function geoip($ip = null)
+    function geoip(string $ip = null): \InteractionDesignFoundation\GeoIP\GeoIP|\InteractionDesignFoundation\GeoIP\Location
     {
+        /** @var \InteractionDesignFoundation\GeoIP\GeoIP $geoip */
+        $geoip = app('geoip');
         if (is_null($ip)) {
-            return app('geoip');
+            return $geoip;
         }
 
-        return app('geoip')->getLocation($ip);
+        return $geoip->getLocation($ip);
     }
 }
