3eb901e11c0e664d66b473e6177246222ffedae7
Mateus <mateus@junges.dev>
2023-03-17 18:31:33 -0300

    Make class final

--- src/Facades/GeoIP.php
+++ src/Facades/GeoIP.php
@@ -4,7 +4,7 @@ namespace InteractionDesignFoundation\GeoIP\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class GeoIP extends Facade
+final class GeoIP extends Facade
 {
     /** Get the registered name of the component. */
     protected static function getFacadeAccessor(): string
