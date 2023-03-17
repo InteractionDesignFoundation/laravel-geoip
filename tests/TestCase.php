ed0ae53e1980ab9e5b6f902e2f12b42db53ed3dd
Mateus <mateus@junges.dev>
2023-03-17 18:29:56 -0300

    Simplify test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace InteractionDesignFoundation\GeoIP\Tests;
 use InteractionDesignFoundation\GeoIP\GeoIP;
 use InteractionDesignFoundation\GeoIP\GeoIPServiceProvider;
 use Mockery;
+use Mockery\MockInterface;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Illuminate\Cache\CacheManager;
 
@@ -22,35 +23,20 @@ class TestCase extends Orchestra
         Mockery::close();
     }
 
-    protected function makeGeoIP(array $config = [], $cacheMock = null): GeoIP
+    protected function makeGeoIP(MockInterface $cacheMock = null): GeoIP
     {
         $cacheMock = $cacheMock ?: Mockery::mock(CacheManager::class);
 
-        $config = array_merge($this->getConfig(), $config);
-
         $cacheMock->shouldReceive('tags')->with(['laravel-geoip-location'])->andReturnSelf();
 
-        return new GeoIP($config, $cacheMock);
-    }
-
-    protected function getConfig(): array
-    {
-        $databasePath = config('geoip.services.maxmind_database.database_path');
-
-        assert(is_string($databasePath));
-
-        $this->databaseCheck($databasePath);
-
-        return config('geoip');
+        return new GeoIP($cacheMock);
     }
 
     /**
      * Check for test database and make a copy of it
      * if it does not exist.
-     *
-     * @param string $database
      */
-    protected function databaseCheck($database)
+    protected function databaseCheck(string $database): void
     {
         if (file_exists($database) === false) {
             @mkdir(dirname($database), 0755, true);
