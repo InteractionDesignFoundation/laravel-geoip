5b91d23af8ea13e9a45f878b3b15a1b3bb9d0484
Mateus <mateus@junges.dev>
2023-03-17 18:31:13 -0300

    Remove unused methods

--- src/Contracts/LocationProvider.php
+++ src/Contracts/LocationProvider.php
@@ -2,7 +2,7 @@
 
 namespace InteractionDesignFoundation\GeoIP\Contracts;
 
-use InteractionDesignFoundation\GeoIP\Location;
+use InteractionDesignFoundation\GeoIP\LocationResponse;
 
 interface LocationProvider
 {
@@ -10,17 +10,5 @@ interface LocationProvider
     public function boot(): void;
 
     /** Determine a location based off of the provided IP address. */
-    public function locate(string $ip): Location;
-
-    /** Create a location instance from the provided attributes. */
-    public function hydrate(array $attributes = []): Location;
-
-    /**
-     * Get configuration value.
-     *
-     * @param mixed  $default
-     *
-     * @return mixed
-     */
-    public function config(string $key, $default = null);
+    public function locate(string $ip): LocationResponse;
 }
