e1667f537a188d35fa151990aca09edcac80ddb2
Alies Lapatsin <web.lapateen@gmail.com>
2026-03-05 18:02:50 +0100

    Fix broken match expression in shouldCache method

--- src/GeoIP.php
+++ src/GeoIP.php
@@ -264,16 +264,13 @@ class GeoIP
      */
     private function shouldCache(Location $location, ?string $ip = null): bool
     {
-        if ($ip === null) {
-            return false;
-        }
-
         if ($location->default === true || $location->cached === true) {
             return false;
         }
 
         return match ($this->config('cache', 'none')) {
-            'all', 'some' && $ip === null => true,
+            'all' => true,
+            'some' => $ip !== null,
             default => false,
         };
     }
